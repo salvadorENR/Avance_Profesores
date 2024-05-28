52233a43086eff6a372d0d31fa2ebbb5d4991989
Kevin Santos <kevinsantos9349@gmail.com>
2024-05-28 10:51:35 -0600

    Add files via upload

---
+++ cargarCSV.R
@@ -0,0 +1,14 @@
+con <- dbConnect(RMySQL::MySQL(),
+                 dbname = "sql3709755",
+                 host = "sql3.freesqldatabase.com",
+                 port = 3306,
+                 user = "sql3709755",
+                 password = "PPK48mhriE")
+
+datos <- read.csv('mis_datos.csv')
+
+# Escribe los datos del csv en la base de datos
+dbWriteTable(con, name = "page_data", value = datos[-1,], row.names = FALSE, overwrite = FALSE)
+
+# Cierra la conexión
+dbDisconnect(con)
