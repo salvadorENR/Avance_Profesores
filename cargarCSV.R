6ba5a2fc79442d6121d3795ae52c4f8f4cf26e8f
Kevin Santos <kevinsantos9349@gmail.com>
2024-05-28 13:44:20 -0600

    Update cargarCSV.R
    Se agregó el query para extraer y subir datos en la base. Además, se agregó para eliminar los datos y para obtener el espacio que utiliza la base de datos.

--- cargarCSV.R
+++ cargarCSV.R
@@ -6,10 +6,28 @@ con <- dbConnect(RMySQL::MySQL(),
                  user = "sql3709755",
                  password = "PPK48mhriE")
 
+##Lee el csv
 datos <- read.csv('mis_datos.csv')
 
 # Escribe los datos del csv en la base de datos
-dbWriteTable(con, name = "page_data", value = datos[-1,], row.names = FALSE, overwrite = FALSE)
+#dbWriteTable(con, name = "page_data", value = datos[-1,], row.names = FALSE, overwrite = FALSE)
 
-# Cierra la conexión
+
+##extraer datos y guardar en csv
+resultados <- dbGetQuery(con, "SELECT * FROM page_data")
+write.csv(resultados, "avance.csv")
+
+# Ejecutar la consulta SQL para eliminar todos los datos
+#dbSendQuery(con, "DELETE FROM page_data")
+
+# Ejecutar la consulta SQL para obtener el tamaño de la tabla "page_data"
+consulta <- "SELECT table_name AS 'Tabla', ROUND(((data_length + index_length) / 1024 / 1024), 2) AS 'Tamaño (MB)'
+             FROM information_schema.tables
+             WHERE table_schema = 'sql3709755' AND table_name = 'page_data'"
+
+tamaño <- dbGetQuery(con, consulta)
+
+# Imprimir el tamaño en MB
+print(tamaño)
+# Cerrar la conexión
 dbDisconnect(con)
