e9ababf0d89d5218822482c32f253504f89e4e23
Salvador Enrique Rodríguez Hernández <salvadorestadistic@gmail.com>
2024-06-10 09:27:16 -0600

    Update app.R

--- app.R
+++ app.R
@@ -18,11 +18,11 @@ initialize_database <- function(con) {
 
 # Connect to MySQL database using RMySQL
 con <- dbConnect(RMySQL::MySQL(),
-                 dbname = "sql3710936",
-                 host = "sql3.freesqldatabase.com",
+                 dbname = "sql5713063",
+                 host = "sql5.freesqldatabase.com",
                  port = 3306,
-                 user = "sql3710936",
-                 password = "clUPkctGN2")
+                 user = "sql5713063",
+                 password = "NMXKA5SSGh")
 
 # Initialize the database and table
 initialize_database(con)
