d1ed83fc84d76ebad3e858c32bf1d63adee83a59
salvadorENR <salvadorestadistic@gmail.com>
2024-10-21 08:06:50 -0600

    Second commit

--- deploy.R
+++ deploy.R
@@ -10,10 +10,10 @@ rsconnect::setAccountInfo(name='salvadorhernandez',
                           secret='Xu/+W3YeXdivSmXgmvOavpZAi6I9TWMGC/Hv+6Fi')
 
 # Set the working directory to where your app.R is located
-setwd("C:/Users/Wendy Rodríguez/Desktop/Avance_Profesores")
+setwd("C:/Users/Wendy Rodríguez/Documents/Avance_Profesores")
 
 # Deploy the application
-rsconnect::deployApp("C:/Users/Wendy Rodríguez/Desktop/Avance_Profesores")
+rsconnect::deployApp("C:/Users/Wendy Rodríguez/Documents/Avance_Profesores")
 
 
 
