a5f6b26a505e8a444b1e2ac751c7bf4096bdc93e
Kevin Santos <kevinsantos9349@gmail.com>
2024-11-14 09:47:00 -0600

    Actuaización de usuario
    Se actualizó al usuario pagado por FUSALMO

--- deploy.R
+++ deploy.R
@@ -5,17 +5,18 @@ install.packages("rsconnect")
 library(rsconnect)
 
 # Set your shinyapps.io account information
-rsconnect::setAccountInfo(name='salvadorhernandez',
-                          token='1BCDF7FEFCE106A56040AC3AF45F2380',
-                          secret='Xu/+W3YeXdivSmXgmvOavpZAi6I9TWMGC/Hv+6Fi')
+rsconnect::setAccountInfo(name='mined',
+                          token='D9838BF3F50E873531DB43A0A6086DD7',
+                          secret='D4ONbu50XZ8keiXTpGtnkLLV7sSJUMfHmKTkNvki')
 
 # Set the working directory to where your app.R is located
-setwd("C:/Users/kevin/OneDrive/Documents/GitHub/Avance_Profesores")
+#setwd("C:/Users/kevin/OneDrive/Documents/GitHub/Avance_Profesores")
 #setwd("C:/Users/Wendy Rodríguez/Documents/Avance_Profesores")
+setwd("C:/Users/KSerrano/Documents/GitHub/Avance_Profesores")
 
 # Deploy the application
-rsconnect::deployApp("C:/Users/kevin/OneDrive/Documents/GitHub/Avance_Profesores")
+#rsconnect::deployApp("C:/Users/kevin/OneDrive/Documents/GitHub/Avance_Profesores")
 #rsconnect::deployApp("C:/Users/Wendy Rodríguez/Documents/Avance_Profesores")
-
+rsconnect::deployApp("C:/Users/KSerrano/Documents/GitHub/Avance_Profesores")
 
 
